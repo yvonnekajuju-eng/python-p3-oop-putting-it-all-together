899a01bc579dd6e0f6846f5e90b6e86ddd153365
yvvonne <yvonnekajuju6@gmail.com>
2025-11-26 10:23:02 +0300

    final commit

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,33 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        if isinstance(value, str):
+            self._title = value
+        else:
+            print("title must be a string")
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            # ✔ TEST EXPECTS PRINT, NOT ERROR
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        # ✔ EXACT STRING REQUIRED BY TEST
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, value):
+        if isinstance(value, str):
+            self._brand = value
+        else:
+            print("brand must be a string")
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            # ✔ TEST EXPECTS PRINT, NOT ERROR
+            print("size must be an integer")
+
+    def cobble(self):
+        # ✔ EXACT STRING REQUIRED BY TEST
+        print("Your shoe is as good as new!")
+        self.condition = "New"
